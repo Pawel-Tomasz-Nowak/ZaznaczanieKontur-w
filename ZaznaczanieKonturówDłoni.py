8776b5c673bc4972eb1a4ede5a5dd26447c2257f
Pawel <pawelnowak2004priv@gmail.com>
2024-06-05 23:17:32 +0200

    Dodanie progowania binarnego, zmiana wymiarów okienka.

--- ZaznaczanieKonturówDłoni.py
+++ ZaznaczanieKonturówDłoni.py
@@ -2,23 +2,41 @@
 import cv2 
 import numpy as np
 
-konwerter = np.array([0.3, 0.587, 0.144])
   
 # define a video capture object 
 vid = cv2.VideoCapture(0) 
-print(type(vid))
-  
+
+#Wymiary okienka.
+window_height:int = 800
+window_width:int = 800
+
+
 while(True): 
       
     # Capture the video frame 
     # by frame 
     ret, frame = vid.read()
 
-
-    grayed_frame = cv2.cvtColor(src = frame, code = cv2.COLOR_RGB2GRAY)
-  
+    #Zacieniuj nasze zdjęcie
+    grayed_frame = cv2.cvtColor(src = frame, 
+                                code = cv2.COLOR_RGB2GRAY)
+    #Zastosuj progowanie binarne.
+    ret, bin_frame = cv2.threshold(src = grayed_frame, 
+                                   thresh = 125,
+                                    maxval = 255, 
+                                    type = cv2.THRESH_BINARY)
+    
+    #Zmień wymiary ramki.
+    bin_frame = cv2.resize(src = bin_frame, 
+                           dsize = (window_width, window_height))
+    
+   
     #Wyświetl w okienku ramkę
-    cv2.imshow('Twoja twarz', grayed_frame) 
+    cv2.imshow('Twoja twarz', bin_frame) 
+
+    cv2.resizeWindow(winname = 'Twoja twarz', 
+                     width = window_width, 
+                     height = window_height)
       
     #Jezeli klikniemy przycisk q, okienko się zamknię.
     if cv2.waitKey(1) & 0xFF == ord('q'): 
